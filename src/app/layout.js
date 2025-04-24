cc010e2e6194aca55e9632de0d149fb903f19182
Gnanadeep Gumpula <gumpulagnanadeep@gmail.com>
2025-04-24 23:19:34 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,15 +5,13 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { Providers } from "./providers";
-
+import webicon from "@/public/favicon.icon";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Green Thicks - Fresh Organic Vegetables",
   description: "Farm to table organic vegetables delivered to your doorstep",
-  icons: {
-    icon: "/favicon.ico",
-  },
+  icons: webicon,
 };
 
 export default function RootLayout({ children }) {
