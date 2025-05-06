032cd6a980ede2332b968e80b8dd1658e3c75c25
GnanadeepGumpula <gumpulagnanadeep@gmail.com>
2025-05-06 20:09:37 +0530

    metadata update

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,9 +10,20 @@ import LayoutWrapper from "@/components/layout-wrapper"; // new wrapper
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Green Thicks - Fresh Organic Vegetables",
-  description: "Farm to table organic vegetables delivered to your doorstep",
-  icons: webicon,
+  title: 'GreenThicks - Fresh Organic Vegetables Delivered to Your Home',
+  description: 'Order fresh, organic vegetables directly from GreenThicks farms to your doorstep. Experience purity, taste, and health with every delivery!',
+  keywords: ['GreenThicks','greenthicks','green thicks','organic vegetables delivery', 'fresh organic vegetables', 'healthy vegetables', 'organic vegetables', 'fresh vegetables', 'vegetable delivery', 'farm fresh vegetables', 'buy vegetables online'],
+  authors: [{ name: 'GreenThicks Team' }],
+  openGraph: {
+    title: 'GreenThicks - Fresh Organic Vegetables',
+    description: 'Farm-fresh organic vegetables delivered to your home. Healthy. Fresh. Pure. Taste the difference!',
+    url: 'https://www.greenthicks.live',
+    siteName: 'GreenThicks',
+    icons: webicon,
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: 'index, follow',
 };
 
 export default function RootLayout({ children }) {
